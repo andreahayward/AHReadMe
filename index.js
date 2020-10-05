18e384f618ced01e20c9851ec9501a3e0849f0f4
Andrea Hayward <andrea.teofilo@yahoo.com>
2020-10-05 12:05:36 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const inquirer = require('inquirer');
+const axios = require("axios");
+const generateMarkdown = require("./generateMarkdown.js");
+const fs = require("fs");
+const questions = [
+    {
+        
+    }
+]
