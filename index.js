cb9096ce1ffc6fa24105b88ead85a5bf29dba571
Andrea Hayward <andrea.teofilo@yahoo.com>
2020-10-05 15:40:00 -0400

    starting questions

--- index.js
+++ index.js
@@ -4,6 +4,34 @@ const generateMarkdown = require("./generateMarkdown.js");
 const fs = require("fs");
 const questions = [
     {
-        
-    }
+        type: "input",
+        message: "Enter the Title for your application/project.",
+        name: "title"
+    },
+    {
+        type: "input",
+        message: "Provide a description of your project.",
+        name: "description",
+    },
+    {
+        type: "input",
+        message: "What is needed to install this application?",
+        name: "installation",
+    },
+    {
+        type: "input",
+        message: "What is this used for?",
+        name: "usage",
+    },
+    {
+        type: "input",
+        message: "credits",
+        name: "credits",
+    },
+    {
+        type: "list",
+        message: "Select which license the user should use.",
+        choices: ["MIT", "IBM", "Apache"],
+        name: "license",
+    },
 ]
