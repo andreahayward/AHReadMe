d8cae621142189a97abf00854c0acf656d40ba22
Andrea Hayward <andrea.teofilo@yahoo.com>
2020-10-07 20:21:30 -0400

    markdown updates

--- generateMarkdown.js
+++ generateMarkdown.js
@@ -3,34 +3,42 @@ function generateMarkdown(data) {
     return `# ${data.title}
 
     ## Application Description
+
     ${data.description}
 
     ## Table of Contents
+
     * [Installation](#installation)
     * [Usage](#usage)
     * [Resource(s)](#resource)
     * [License](#license)
 
     ## Installation
-    The following items/steps are required to install this application:
+
     ${data.installation}
 
     ## Resource(s)
+
     ${data.resource}
 
     ## License
+
     ${data.license ==="MIT"?"[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)":data.license==="IBM"?"[![License: IPL 1.0](https://img.shields.io/badge/License-IPL%201.0-blue.svg)](https://opensource.org/licenses/IPL-1.0)":"[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)"}
 
     ## Badge
+
     ${data.badge==="Last Commit"?"[![GitHub last comm](https://img.shields.io/github/last-commit/google/skia.svg?style=flat)]()":data.badge==="Issues"?"[![GitHub Issues](https://img.shields.io/github/issues-pr/cdnjs/cdnjs.svg?style=flat)]()":"[![Issues](https://img.shields.io/github/issues-raw/tterb/PlayMusic.svg?maxAge=25000)](https://github.com/tterb/Hyde/issues)"}
 
     ## Contributors
+
     ${data.contributors}
 
     ## Testing
+
     ${data.test}
 
     ## Contact Info
+    
     *For any additional questions or support please reach out:
     ${data.username}
     ${data.email}
